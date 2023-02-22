4a402e469970fcbbbd82e51fbf4dc14246dac637
yessasikiran <sxs73800@ucmo.com>
2023-02-21 19:57:05 -0600

    beta

---
+++ main.py
@@ -0,0 +1,21 @@
+import configparser
+import file_reader
+
+# Read configuration
+config = configparser.ConfigParser()
+config.read('config.ini')
+source_filename = config['source']['filename']
+
+# Read source file
+source_content = file_reader.read_file(source_filename)
+destination_filename = config['destination']['filename']
+
+# Parse source content
+lines = source_content.splitlines()
+keys = lines[0].split(',')
+values = lines[1].split(',')
+
+# Write to destination file
+with open(destination_filename, 'w') as f:
+    f.write(','.join(keys) + '\n')
+    f.write(','.join(values) + '\n')
